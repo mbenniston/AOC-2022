53e6a48f0e7d4c9916a67560eb0034cb5eab22a3
mbenniston <matthewpbenniston@gmail.com>
2022-12-06 01:01:08 +0000

    Added day05 solutions

---
+++ day-05/day05_p1.exs
@@ -0,0 +1,152 @@
+defmodule CrateMoving do
+  # Moves a single crate from the from slot to the to slot
+  def move_single_crate(state, fromSlot, toSlot) do
+    Enum.with_index(state)
+    |> Enum.map(fn {slots, index} ->
+      cond do
+        index == fromSlot ->
+          List.delete_at(slots, 0)
+
+        index == toSlot ->
+          List.insert_at(slots, 0, List.first(Enum.at(state, fromSlot)))
+
+        true ->
+          slots
+      end
+    end)
+  end
+
+  # Run move crates multiple times feeding the output of the previous move
+  # into the next
+  def move_crates(state, numCrates, fromSlot, toSlot) do
+    if numCrates == 0 do
+      state
+    else
+      next_state = move_single_crate(state, fromSlot, toSlot)
+      move_crates(next_state, numCrates - 1, fromSlot, toSlot)
+    end
+  end
+
+  # Apply state creation lines to create an initial state
+  def construct_initial_state(initial_state_lines, num_stacks) do
+    parsed_initial_state_lines =
+      initial_state_lines
+      |> Enum.map(fn line -> parse_state_creation_line(line, num_stacks) end)
+
+    for i <- 0..(num_stacks - 1) do
+      parsed_initial_state_lines
+      |> Enum.map(fn state_line -> Enum.at(state_line, i) end)
+      |> Enum.filter(fn item -> item != " " end)
+    end
+  end
+
+  # Gets the number line that labels the crates
+  def get_number_line(lines) do
+    if(length(lines) == 0) do
+      nil
+    else
+      line = List.first(lines)
+
+      if(not String.contains?(line, "[")) do
+        line
+      else
+        get_number_line(List.delete_at(lines, 0))
+      end
+    end
+  end
+
+  # Gets the initial crate configuration lines
+  def get_initial_state_lines(lines, accumlated_lines) do
+    if(length(lines) == 0) do
+      nil
+    else
+      line = List.first(lines)
+
+      if(String.contains?(line, "[")) do
+        new_lines = List.insert_at(accumlated_lines, length(accumlated_lines), line)
+        get_initial_state_lines(List.delete_at(lines, 0), new_lines)
+      else
+        accumlated_lines
+      end
+    end
+  end
+
+  # Gets the move crate lines
+  def get_instruction_lines(lines) do
+    if(length(lines) == 0) do
+      nil
+    else
+      line = List.first(lines)
+
+      if(String.contains?(line, "from")) do
+        lines
+      else
+        get_instruction_lines(List.delete_at(lines, 0))
+      end
+    end
+  end
+
+  # Returns the number of stacks given a number line
+  def num_stacks(number_line) do
+    Integer.floor_div(String.length(number_line) + 1, 4)
+  end
+
+  # Creates a list of items from a creation line
+  def parse_state_creation_line(line, numStacks) do
+    for i <- 0..(numStacks - 1) do
+      String.at(line, 1 + i * 4)
+    end
+  end
+
+  # Parses a single instruction line
+  def parse_line(line) do
+    tokens = String.split(line, " ")
+    ["move", numCratesStr, "from", fromSlotStr, "to", toSlotStr] = tokens
+
+    {numCrates, _} = Integer.parse(numCratesStr)
+    {fromSlot, _} = Integer.parse(fromSlotStr)
+    {toSlot, _} = Integer.parse(toSlotStr)
+    {numCrates, fromSlot, toSlot}
+  end
+
+  def parse_and_execute_instructions(lines, prevState) do
+    if length(lines) == 0 do
+      prevState
+    else
+      {line, nextLines} = List.pop_at(lines, 0)
+      {numCrates, fromSlot, toSlot} = parse_line(line)
+
+      nextState = move_crates(prevState, numCrates, fromSlot - 1, toSlot - 1)
+      parse_and_execute_instructions(nextLines, nextState)
+    end
+  end
+end
+
+# Read input file
+{:ok, contents} = File.read("input.txt")
+
+# Split into lines and remove whitespace
+lines = String.split(contents, "\n") |> Enum.map(fn s -> String.replace_trailing(s, "\r", "") end)
+
+# Get the number of crates in the initial state
+number_line = CrateMoving.get_number_line(lines)
+num_stacks = CrateMoving.num_stacks(number_line)
+
+# Get the line in the file related to creating the initial state
+initial_state_lines = CrateMoving.get_initial_state_lines(lines, [])
+
+# Create the initial state
+initial_state = CrateMoving.construct_initial_state(initial_state_lines, num_stacks)
+
+# Collect and execute instruction lines
+instruction_lines =
+  CrateMoving.get_instruction_lines(lines) |> Enum.filter(fn line -> line != "" end)
+
+outputState = CrateMoving.parse_and_execute_instructions(instruction_lines, initial_state)
+
+# Fetch top item of each crate stack and print it out
+IO.puts(
+  outputState
+  |> Enum.map(fn crateStack -> List.first(crateStack) end)
+  |> Enum.reduce("", fn crate, acc -> acc <> crate end)
+)
